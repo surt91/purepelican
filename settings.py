413b36f5af7ff497f97887d41f1ad707297426f8
Hendrik Schawe <surt91@gmail.com>
2017-08-27 18:08:59 +0200

    added the time of the build

--- settings.py
+++ settings.py
@@ -3,3 +3,6 @@ DIRECT_TEMPLATES = (('index', 'tags', 'categories', 'archives', 'search', '404')
 ASSET_BUNDLES = (
     ('sass', ['sass/blog.scss'], {'filters': 'libsass', 'output': 'sass.css'}),
 )
+
+from datetime import datetime
+NOW = datetime.now()
