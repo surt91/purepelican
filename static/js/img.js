0cbad630c23e7f0ba93b597ac799fd704981d5fc
Hendrik Schawe <surt91@gmail.com>
2017-11-11 12:44:57 +0100

    simplify the image loading logic

--- static/js/img.js
+++ static/js/img.js
@@ -1,22 +1,15 @@
+// inspired by
 // https://jmperezperez.com/medium-image-progressive-loading-placeholder/
 
 function set_image(ph) {
     let small = ph.querySelector('.img-small');
 
-    // 1: load small image and show it
-    let img = new Image();
-    img.src = small.src;
-    img.onload = function () {
-        small.classList.add('loaded');
-    };
-
-    // 2: load large image
     let imgLarge = new Image();
-    imgLarge.style.position = "absolute";
     imgLarge.alt = small.alt;
     imgLarge.src = ph.dataset.large;
+    imgLarge.classList.add("img-large")
     imgLarge.onload = function () {
-        imgLarge.classList.add('loaded');
+        imgLarge.classList.add("loaded")
         small.style.opacity = 0;
         ph.style.backgroundColor = "";
     };
