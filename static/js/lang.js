8feb84acf8444da099f35021e45bfe3bb3470cf4
Hendrik Schawe <surt91@gmail.com>
2019-10-20 19:12:35 +0200

    removed calls of non-existing functions

--- static/js/lang.js
+++ static/js/lang.js
@@ -46,13 +46,10 @@ function init_language(current) {
         // nothing known saved, guess language
         var userLang = navigator.language || navigator.userLanguage;
         if (userLang.substring(0, 1).toLowerCase() == "de") {
-            switch_language_de();
             window.location.replace("/de/");
         } else if (userLang.substring(0, 1).toLowerCase() == "fr") {
-            switch_language_fr();
             window.location.replace("/fr/");
         } else {
-            switch_language_en();
             // we are already on the default, english page, so do nothing
         }
     }
