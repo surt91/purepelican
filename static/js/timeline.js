13a0f1a31de03ee36b021a5ccb16417d47847e88
Hendrik Schawe <surt91@gmail.com>
2020-07-10 15:18:46 +0200

    choose which categories of the timeline to show

---
+++ static/js/timeline.js
@@ -0,0 +1,61 @@
+const categories = ["work", "visit", "graduation", "conference", "workshop", "summerschool"]
+const cat_defaults = {
+    work: "true",
+    visit: "true",
+    graduation: "true",
+    conference: "true",
+    workshop: "false",
+    summerschool: "false"
+}
+
+document.addEventListener("DOMContentLoaded", init_timeline);
+
+function init_timeline() {
+    for (let cat of categories) {
+        let visibility = localStorage.getItem(cat);
+        if (visibility === null) {
+            // apparently undefined, take a default value
+            localStorage.setItem(cat, cat_defaults[cat]);
+            visibility = cat_defaults[cat];
+        }
+
+        if (visibility === "true") {
+            // remove class hidden
+            document.querySelector('[data-category="' + cat + '"]').checked = true;
+            show_timeline(cat);
+        } else if (visibility === "false") {
+            // append class hidden
+            document.querySelector('[data-category="' + cat + '"]').checked = false;
+            hide_timeline(cat);
+        }
+    }
+
+    let tl_boxes = document.getElementsByClassName("timeline-checkbox");
+    for (let chkbx of tl_boxes) {
+        chkbx.addEventListener( 'change', function() {
+            if(this.checked) {
+                show_timeline(this.dataset.category);
+            } else {
+                hide_timeline(this.dataset.category);
+            }
+        });
+    }
+}
+
+function show_timeline(category) {
+    localStorage.setItem(category, "true");
+    console.log("hide", category);
+    for (let item of document.getElementsByClassName(category)) {
+        console.log("show", item);
+        item.classList.remove("hidden");
+    }
+}
+
+function hide_timeline(category) {
+    localStorage.setItem(category, "false");
+    console.log("hide", category);
+    for (let item of document.getElementsByClassName(category)) {
+        console.log("hide", item);
+        item.classList.add("hidden");
+    }
+}
