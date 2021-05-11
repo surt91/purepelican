d363541e38926e51f9218d2c15e3fa374edd5894
Hendrik Schawe <surt91@gmail.com>
2021-05-11 19:34:07 +0200

    updated search

--- static/tipuesearch/tipuesearch_set.js
+++ static/tipuesearch/tipuesearch_set.js
@@ -1,7 +1,7 @@
 
 /*
-Tipue Search 6.1
-Copyright (c) 2017 Tipue
+Tipue Search 7.1
+Copyright (c) 2019 Tipue
 Tipue Search is released under the MIT License
 http://www.tipue.com/search
 */
@@ -13,7 +13,6 @@ Stop words list from http://www.ranks.nl/stopwords
 */
 
 var tipuesearch_stop_words = ["aber", "als", "am", "an", "auch", "auf", "aus", "bei", "bin", "bis", "bist", "da", "dadurch", "daher", "darum", "das", "daß", "dass", "dein", "deine", "dem", "den", "der", "des", "dessen", "deshalb", "die", "dies", "dieser", "dieses", "doch", "dort", "du", "durch", "ein", "eine", "einem", "einen", "einer", "eines", "er", "es", "euer", "eure", "für", "hatte", "hatten", "hattest", "hattet", "hier", "hinter", "ich", "ihr", "ihre", "im", "in", "ist", "ja", "jede", "jedem", "jeden", "jeder", "jedes", "jener", "jenes", "jetzt", "kann", "kannst", "können", "könnt", "machen", "mein", "meine", "mit", "muß", "mußt", "musst", "müssen", "müßt", "nach", "nachdem", "nein", "nicht", "nun", "oder", "seid", "sein", "seine", "sich", "sie", "sind", "soll", "sollen", "sollst", "sollt", "sonst", "soweit", "sowie", "und", "unser", "unsere", "unter", "vom", "von", "vor", "wann", "warum", "was", "weiter", "weitere", "wenn", "wer", "werde", "werden", "werdet", "weshalb", "wie", "wieder", "wieso", "wir", "wird", "wirst", "wo", "woher", "wohin", "zu", "zum", "zur", "über"];
-
 
 // Word replace
 
@@ -27,9 +26,6 @@ var tipuesearch_replace = {'words': [
 // Weighting
 
 var tipuesearch_weight = {'weight': [
-     {'url': 'http://www.tipue.com', 'score': 20},
-     {'url': 'http://www.tipue.com/search', 'score': 30},
-     {'url': 'http://www.tipue.com/is', 'score': 10}
 ]};
 
 
@@ -42,13 +38,9 @@ var tipuesearch_stem = {'words': [
 ]};
 
 
-// Related searches
+// Related
 
-var tipuesearch_related = {'searches': [
-     {'search': 'tipue', 'related': 'Tipue Search'},
-     {'search': 'tipue', 'before': 'Tipue Search', 'related': 'Getting Started'},
-     {'search': 'tipue', 'before': 'Tipue', 'related': 'jQuery'},
-     {'search': 'tipue', 'before': 'Tipue', 'related': 'Blog'}
+var tipuesearch_related = {'Related': [
 ]};
 
 
@@ -77,3 +69,4 @@ var tipuesearch_string_15 = 'Suche im Zusammenhang mit';
 // Timer for showTime
 
 var startTimer = new Date().getTime();
+
