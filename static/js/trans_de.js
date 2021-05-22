7a4adde4bf18e2d4ff7e516d2422d4962e270870
Hendrik Schawe <surt91@gmail.com>
2021-05-22 11:05:45 +0200

    use lunr instead of tipue
    because tipue seems to be dead

---
+++ static/js/trans_de.js
@@ -0,0 +1,2 @@
+const result_title_str = "Suchergebnisse für";
+const fail_str = "Keine Suchergebnisse für";
---
+++ static/js/trans_en.js
@@ -0,0 +1,2 @@
+const result_title_str = "Search results for";
+const fail_str = "No results found for";
