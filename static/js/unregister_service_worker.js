2c04e7e485b756a3a43db7652c69f9d2547b184a
Hendrik Schawe <surt91@gmail.com>
2021-05-23 13:23:11 +0200

    unregister the old unused service worker
    otherwise people will always se an old version of the website

---
+++ static/js/unregister_service_worker.js
@@ -0,0 +1,3 @@
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.getRegistrations().then( function(registrations) { for(let registration of registrations) { registration.unregister(); } });
+}
